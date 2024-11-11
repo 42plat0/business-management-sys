64d862f32eb9d57ea2fc00d2d93e5fb629f2d906
Gypra <gytisnaujasis@icloud.com>
2024-11-11 16:00:03 +0200

    Update forms to hold placeholders instead of labels

--- managementsystem/blueprints/auth/forms.py
+++ managementsystem/blueprints/auth/forms.py
@@ -5,20 +5,26 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegisterForm(FlaskForm):
     username = StringField("Username",
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)],
+                           render_kw={"placeholder": "Username..."})
     email = EmailField("Email",
-                       validators=[DataRequired()])
+                       validators=[DataRequired()],
+                       render_kw={"placeholder": "Email..."})
     password = PasswordField("Password",
-                             validators=[DataRequired()])
+                             validators=[DataRequired()],
+                             render_kw={"placeholder": "Password..."})
     confirm_password = PasswordField("Confirm Password",
-                                     validators=[DataRequired(), EqualTo("password", "Passwords do not match!")])
+                                     validators=[DataRequired(), EqualTo("password", "Passwords do not match!")],
+                                     render_kw={"placeholder": "Confirm Password..."})
     submit = SubmitField("Register")
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username",
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)],
+                           render_kw={"placeholder": "Username..."})
     password = PasswordField("Password",
-                             validators=[DataRequired()])
+                             validators=[DataRequired()],
+                             render_kw={"placeholder": "Password..."})
     submit = SubmitField("Login")
     
