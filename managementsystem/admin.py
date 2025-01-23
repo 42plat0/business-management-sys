f6f0bfaabe89b1c8c720c46e280dc7a67fea4984
Gypra <gytisnaujasis@icloud.com>
2025-01-23 14:07:26 +0200

    Restrict admin view for those, who aren't authenticated. TODO later update it to only admin access

--- managementsystem/admin.py
+++ managementsystem/admin.py
@@ -1,8 +1,29 @@
-from flask_admin import Admin
+import flask_login
+from flask import redirect, url_for
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 
 from .blueprints.auth.models import User
 
 def create_admin(app, db):
-    admin = Admin(app, name="cafe")
-    admin.add_view(ModelView(User, db.session))
+    admin = Admin(app, index_view=CafeAdminIndexView(), name="cafe")
+    admin.add_view(CafeModelView(User, db.session))
+
+class CafeModelView(ModelView):
+    
+    def is_accessible(self):
+        return flask_login.current_user.is_authenticated
+    
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('blueprints.auth.login'))
+
+class CafeAdminIndexView(AdminIndexView):
+    @expose("/")
+    def index(self):
+        if not flask_login.current_user.is_authenticated:
+            return redirect(url_for('auth.login'))
+        
+        return super().index()
+  
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login'))
